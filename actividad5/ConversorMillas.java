61f1443bbd62936ad8f0ff1a238a12a2e774e3cb
RoynerG <roynergh@hotmail.com>
2023-06-06 16:46:08 -0500

    add: ejercicio 2 - actividad 5

---
+++ actividad5/ConversorMillas.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ConversorMillas {
+  public static void main(String[] args) {
+    Scanner leer = new Scanner(System.in);
+    System.out.println("Bienvenido a la aplicacion para convertir de millas a KM");
+    System.out.println("Ingrese una distancia en millas:");
+    double millas = leer.nextDouble();
+    double km = millas * 1.60934;
+    System.out.println("La distancia en kilometros es: "+km);
+  }
+}
