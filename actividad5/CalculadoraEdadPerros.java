bd23965ce84c3d6a638651d0eded621030d629a5
RoynerG <roynergh@hotmail.com>
2023-06-06 16:40:14 -0500

    add: ejercicio 1 - actividad 5

---
+++ actividad5/CalculadoraEdadPerros.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CalculadoraEdadPerros {
+
+  public static void main(String[] args) {
+    Scanner leer = new Scanner(System.in);
+    System.out.println("Ingresa la edad de tu perro: ");
+    int edad = leer.nextInt();
+    int edadPerro = edad * 7;
+    System.out.println("La edad de tu perro es: "+edadPerro);
+    
+    }
+}
