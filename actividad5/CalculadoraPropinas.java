824e42928a09c37511b3d728566746adc03fd427
RoynerG <roynergh@hotmail.com>
2023-06-06 17:09:36 -0500

    add: ejercicio 4 - actividad 5

---
+++ actividad5/CalculadoraPropinas.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CalculadoraPropinas {
+  public static void main(String[] args) {
+    Scanner leer = new Scanner(System.in);
+    System.out.println("Ingresa la cuenta total de su comida: ");
+    double totalCuenta = leer.nextDouble();
+    System.out.println("Ingrese el porcentaje de propina que desee dar: ");
+    double porcentajePropina = leer.nextDouble();
+    double propina = totalCuenta * (porcentajePropina / 100);
+    System.out.println("La propina total que dará al mesero es de: "+propina);
+
+  }
+}
