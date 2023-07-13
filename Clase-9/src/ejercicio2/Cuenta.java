932adfc111d42965e52360be3030a49e3dd1ee22
RoynerG <roynergh@hotmail.com>
2023-07-13 17:44:47 -0500

    ejercicio 2 actualizado

--- Clase-9/src/ejercicio2/Cuenta.java
+++ Clase-9/src/ejercicio2/Cuenta.java
@@ -3,4 +3,45 @@ package ejercicio2;
 public class Cuenta {
     private String titular;
     private double cantidad;
+
+    public Cuenta() {
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void mostrar(){
+        System.out.println("Acontinuación se mostrarán tus datos de cuenta bancaria: ");
+        System.out.println("Titular de la cuenta: "+ this.titular);
+        System.out.println("Cantidad disponible en la cuenta: $ "+ this.cantidad);
+    }
+
+    public void ingresar(double cantidad){
+        if (cantidad < 0 ){
+            System.out.println("No se puede ingresar numero negativos");
+        }
+        else{
+            System.out.println("La cantidad: $"+cantidad+ " fue ingresada con éxito.");
+            cantidad+=cantidad;
+
+        }
+    }
+
+    public void retirar(double cantidad){
+        System.out.println("La cantidad: $"+cantidad+ " fue retirada con éxito.");
+        cantidad-=cantidad;
+    }
 }
---
+++ Clase-9/src/ejercicio2/PruebaCuenta.java
@@ -0,0 +1,10 @@
+package ejercicio2;
+
+import java.util.Scanner;
+
+public class PruebaCuenta {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingresa la cantidad a tu cuenta: ");
+    }
+}
